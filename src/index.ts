3de6e2da37649339b099f847715b04ad884cfc75
Chocolatiines <pimooshwoom@gmail.com>
2024-02-19 17:43:38 +0100

    Exos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
+import {inspect} from "util";
 
 const doctorsData = [
   {
@@ -13,6 +14,8 @@ const doctorsData = [
     speciality: 'PSYCHOLOGIST',
   },
 ];
+
+const colorsData = ["#FF5733", "#33FF57", "#3357FF"]
 
 const typeDefs = `#graphql
 type Doctor {
@@ -28,23 +31,80 @@ type Address {
 
 enum SPECIALITY {
   PSYCHOLOGIST
-  OPHTALMOLOGIST
+  OPHTHALMOLOGIST
+}
+
+enum OPERATION {
+    ADD
+    SUBTRACT
+    MULTIPLY
+    DIVIDE
 }
 
 type Query {
-  doctors: [Doctor]
+  doctors(specialities: [SPECIALITY!]): [Doctor]
   doctor(id: ID!): Doctor
+  calcul(a: Float, b: Float, operation: [OPERATION!]): Float
+  closestColor(hex: String): String
 }
 `
+
 
 const resolvers = {
   Query: {
     doctors: (parent, args, context, info) => {
-      return doctorsData
+        const {specialities} = args
+        if (specialities) {
+          return doctorsData.filter(d => specialities.includes(d.speciality))
+        }
     },
     doctor: (parent, args, context, info) => {
       const {id} = args
       return doctorsData.find(d => d.id === id)
+    },
+    calcul: (parent, args, context, info) => {
+      const {a, b, operation} = args
+      let result = a
+      operation.forEach(op => {
+        switch (op) {
+          case 'ADD':
+            result += b
+            break
+          case 'SUBTRACT':
+            result -= b
+            break
+          case 'MULTIPLY':
+            result *= b
+            break
+          case 'DIVIDE':
+            result /= b
+            break
+        }
+      })
+      return result
+    },
+    closestColor: (parent, args, context, info) => {
+      const {hex} = args
+      let closest : number = Infinity;
+      let index : number;
+
+      const r = parseInt(hex.slice(1, 3), 16)
+      const g = parseInt(hex.slice(3, 5), 16)
+      const b = parseInt(hex.slice(5, 7), 16)
+
+      colorsData.forEach((c, i) => {
+        const r2 = parseInt(c.slice(1, 3), 16)
+        const g2 = parseInt(c.slice(3, 5), 16)
+        const b2 = parseInt(c.slice(5, 7), 16)
+
+        const distance = Math.sqrt((r - r2) ** 2 + (g - g2) ** 2 + (b - b2) ** 2)
+
+        if (distance < closest) {
+          closest = distance
+          index = i
+        }
+      });
+      return colorsData[index];
     }
   },
   Doctor: {
